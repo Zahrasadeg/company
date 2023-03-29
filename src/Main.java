d1b3b842a32ab2e9000095bda73eb5ccac00bd4c
Zahrasadeg <sadeghiz071@gmail.com>
2023-03-29 17:57:55 -0400

    i missed iy up

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        company company=new company();
+        company.name="google";
+        company.owner="asdd";
+        company.year=1995;
+        software soft=new software();
+        soft.softwareName="sdfgf";
+        soft.yearmade=2022;
 
     }
 }
